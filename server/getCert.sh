3d603f01dd4c047b481d11ef1373629dd80d880c
IMcPwn <imcpwn@protonmail.com>
2016-05-30 12:48:35 -0400

    Add getCert. Add exception handling for configuration. Add print_error() and print_notice() functions. Make commands a hash. Close #9. Close #6. Close #3.

---
+++ server/getCert.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Carleton Stuberg - http://imcpwn.com
+# BrowserBackdoorServer by IMcPwn.
+# See the file 'LICENSE' for copying permission
+#
+
+set -e
+
+echo "Deleting old certbot"
+rm -rf certbot-auto
+
+echo "Downloading certbot"
+wget https://dl.eff.org/certbot-auto
+chmod a+x certbot-auto
+
+printf "Enter the domain for the certificate: "
+read -r domain
+
+./certbot-auto certonly --standalone --domain $domain
+
+echo "Creating symlinks for privkey.pem and cert.pem"
+if [[ -e $(pwd)/privkey.pem || -h $(pwd)/privkey.pem ]]
+then
+    printf "privkey.pem already exists. Delete? y | n: "
+    read -r deletePrivKey
+    if [[ $deletePrivKey == "y" ]]
+    then
+        rm -rf $(pwd)/privkey.pem
+    else
+        echo "Quitting"
+        exit
+    fi
+fi
+if [[ -e $(pwd)/cert.pem || -h $(pwd)/cert.pem ]]
+then
+    printf "cert.pem already exists. Delete? y | n: "
+    read -r deleteCert
+    if [[ $deleteCert == "y" ]]
+    then
+        rm -rf $(pwd)/cert.pem
+    else
+        echo "Quitting"
+        exit
+    fi
+fi
+ln -s /etc/letsencrypt/live/$domain/privkey.pem $(pwd)
+ln -s /etc/letsencrypt/live/$domain/cert.pem $(pwd)
+echo "privkey.pem and cert.pem should be in the current directory."
