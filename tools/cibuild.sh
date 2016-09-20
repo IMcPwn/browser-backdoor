a96a04093823e89cc6112a0bc7dcee9a031fc98b
IMcPwn <cs@imcpwn.com>
2016-09-20 15:35:14 -0400

    Remove client from travis testing

--- tools/cibuild.sh
+++ tools/cibuild.sh
@@ -7,15 +7,6 @@
 
 set -e
 
-echo "Entering client directory"
-cd client
-echo "Installing npm dependencies"
-npm install
-echo "Running electron-packager . --all"
-electron-packager . --all
-echo "Returning to root of project"
-cd -
-
 echo "Entering server directory"
 cd server
 echo "Installing ruby dependencies"
