d24bb9070fc1ac81c64336bd9bd208959c8d5075
IMcPwn <cs@imcpwn.com>
2016-10-01 14:10:11 -0400

    Improve error messages of modules. Add createFileForce module.

--- server/modules/downloadFile.js
+++ server/modules/downloadFile.js
@@ -12,18 +12,18 @@ fs = require('fs');
 
 /**
  * @param {String} fullPath - The full path of the file to download.
- * @param {String} encoding - The encoding type of the file.
+ * @param {String} encoding - The encoding type of the data.
  * @return {String} |error|
  * @return {String} $data - The downloaded file.
  */
 downloadFile = function (fullPath, encoding) {
     fs.readFile(fullPath, encoding, function (err, data) {
         if (err) {
-            ws.send("Error: " + err.toString());
-            return;
+            ws.send("Error downloading file: " + err.toString());
+        } else {
+            ws.send(data);
         }
-        ws.send(data);
     });
 }
 
-ws.send("\nUsage: downloadFile(fullPath, encoding)\n- fullPath is the properly escaped full path of the file.\n- encoding is a string of the encoding type of the file. E.g. \"utf8\".");
+ws.send("\nUsage: downloadFile(fullPath, encoding)\n- fullPath is the properly escaped full path of the file.\n- encoding is a string of the encoding type of the data. E.g. \"utf8\".");
